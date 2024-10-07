f09a4a8e36caed0e59bc45be6b87c765082c7cea
BounyMathieu <mathieu.bouny@edu.esiee.fr>
2024-10-07 13:21:35 +0000

    un message explicatif

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World !");
-    return 0;
+#define carre1(X) X*X
+#define carre2(X) X*(X)
+#define carre3(X) (X)*(X)
+
+int main()
+{
+    int n = 5;
+    printf("%d\n", carre1(n+3) );
+    printf("%d\n", carre2(n+3) );
+    printf("%d\n", carre3(n+3) );
 }
